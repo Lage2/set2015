f150851c17e3be3d886f94576779b9606f2bba91
MiguelSantos1020 <habiki_tokogawa@hotmail.com>
2014-11-26 15:52:25 +0000

    Adicionados dummy containers para oradores e javascript correspondente

---
+++ js/oradorFlipScript.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    /* The following code is executed once the DOM is loaded */
+
+    $('.oradorFlip').mouseenter(function(){
+
+
+        // $(this) point to the clicked .oradorFlip element (caching it in elem for speed):
+
+        var elem = $(this);
+
+        // data('flipped') is a flag we set when we flip the element:
+
+        
+        if(elem.data('flipped')){}
+        else
+        {
+            elem.fadeTo("fast", 0.2);
+            elem.siblings('.oradorData').slideToggle(1500);
+            elem.data("flipped",true);
+        }
+
+            // Setting the flag:
+        
+        });
+
+    $('.oradorFlip').mouseleave(function(){
+        var elem = $(this);
+
+            if(elem.data('flipped')){
+                elem.fadeTo(1000, 1.0);
+                elem.siblings('.oradorData').slideToggle(1000);
+                elem.data("flipped",false); 
+            }
+        });
+});
