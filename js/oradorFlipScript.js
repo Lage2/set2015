9649ad253807f00074dc5a7d1d0ec6cb7dd6fad2
rodrigojmlourenco <rodrigo.jm.lourenco@gmail.com>
2014-11-26 17:55:05 +0000

    correcções ao js e dom

--- js/oradorFlipScript.js
+++ js/oradorFlipScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     /* The following code is executed once the DOM is loaded */
 
-    $('.oradorFlip').mouseenter(function(){
+    $('.oradorTrigger').mouseenter(function(){
 
 
         // $(this) point to the clicked .oradorFlip element (caching it in elem for speed):
@@ -14,8 +14,7 @@ $(document).ready(function(){
         if(elem.data('flipped')){}
         else
         {
-            elem.stop(true,true).fadeTo(1500, 0.2);
-            elem.siblings('.oradorData').slideToggle(1500);
+            elem.siblings('.oradorData').stop(true,true).slideToggle("slow");
             elem.data("flipped",true);
         }
 
@@ -23,12 +22,11 @@ $(document).ready(function(){
         
         });
 
-    $('.oradorFlip').mouseleave(function(){
+    $('.oradorTrigger').mouseleave(function(){
         var elem = $(this);
 
             if(elem.data('flipped')){
-                elem.stop(true,true).fadeTo(1000, 1.0);
-                elem.siblings('.oradorData').slideToggle(1000, function(){ var x = $(this); x.clearQueue(); });
+                elem.siblings('.oradorData').stop(true,true).slideToggle(1000);
                 elem.data("flipped",false); 
             }
         });
