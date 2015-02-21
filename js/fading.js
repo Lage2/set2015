ff63177b00391cb59487d2b8e901008a13883c8a
vitopedro <pedrogilvito@gmail.com>
2015-02-21 03:03:19 +0000

    pagina calendario nova versão
    nova versão da nova pagina calendario

--- js/fading.js
+++ js/fading.js
@@ -2,6 +2,7 @@
 
 //GLOBAL Variable
 active_option = null;
+dia_ativo = 2;
 
 function sinple_fade_inout(fadein)
 {
@@ -49,3 +50,22 @@ function change_src(id, src)
 	alert("change_src");
 	document.getElementById(id).src = src;			
 }
+
+
+function show_dia(n)
+{
+	if(n != dia_ativo)
+	{
+		document.getElementById("dia_"+dia_ativo).className = "agenda-date";
+		document.getElementById("dia_"+n).className = "agenda-date dia_selected";
+		
+		//$('#dia_container_'+dia_ativo).fadeOut("fast").css("display", "none");
+		//$('#dia_container_'+n).fadeIn("fast").css("display", "table-cell");
+		
+		document.getElementById("dia_container_"+dia_ativo).className = "dia_container";
+		document.getElementById("dia_container_"+n).className = "dia_container dia_container_selected";
+		
+		dia_ativo = n;
+	}
+}
+
